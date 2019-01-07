8ee53c8ec8b4e78895454598bd93a519d017b8d7
Dustin Ketterman <ketterman.d@gmail.com>
2019-01-06 20:29:51 -0500

    Initial commit

---
+++ goodbye.js
@@ -0,0 +1,11 @@
+exports.goodbyeEnglish = function(){
+  console.log("Goodbye");
+}
+
+exports.goodbyeFrench = function(){
+  console.log("Au revoir");
+}
+
+exports.goodbyeSpanish = function(){
+  console.log("Adios");
+}
